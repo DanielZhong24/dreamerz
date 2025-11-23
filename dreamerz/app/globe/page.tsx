702769826bee08e972d7bd7c9d1d84f002ea4804
andrz25 <andrz25@my.yorku.ca>
2025-11-23 12:39:03 -0500

    globe initial setup

---
+++ dreamerz/app/globe/page.tsx
@@ -0,0 +1,8 @@
+import DreamGlobe from "@/components/dream-globe";
+export default function GlobePage() {
+  return (
+    <div>
+      <DreamGlobe />
+    </div>
+  )
+}
---
+++ dreamerz/components/dream-globe.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Globe from 'react-globe.gl';
+import { useEffect, useState } from 'react';
+
+interface PointData {
+  name: string;
+  lat: number;
+  lng: number;
+}
+
+export default function DreamGlobe() {
+  const [points, setPoints] = useState<PointData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+    }
+
+    fetchData();
+  }, [])
+
+  return (
+    < div className="w-full h-full">
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+        backgroundColor="#000000"
+        animateIn={true}
+
+        pointsData={points}
+      />
+    </div >
+  )
+}
