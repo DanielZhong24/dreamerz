838e08bc297e6c01915d60784a6ccfa17516b737
Andrew Cao <andrewcao618@gmail.com>
2025-11-23 21:27:03 +0000

    testing audio

--- dreamerz/components/VoiceForm.tsx
+++ dreamerz/components/VoiceForm.tsx
@@ -1,14 +1,6 @@
 'use client';
 
-import { useState } from 'react';
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from 'react-speech-recognition';
-import dynamic from 'next/dynamic';
-const LiveAudioVisualizer = dynamic(
-  () => import('react-audio-visualize').then((mod) => mod.LiveAudioVisualizer),
-  { ssr: false }
-);
+import useSpeechRecognition from '../hooks/useSpeechRecognition';
 import { Button } from './ui/button';
 import {
   Dialog,
@@ -23,53 +15,14 @@ import {
 import { AudioLines, Mic } from 'lucide-react';
 
 export default function VoiceForm() {
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
-    null
-  );
-  const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
+  const { text, listening, startListening, stopListening, hasSupport } =
+    useSpeechRecognition();
 
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
-
-  const startRecording = async () => {
-    try {
-      const audio = await navigator.mediaDevices.getUserMedia({ audio: true });
-      setAudioStream(audio);
-
-      const recorder = new MediaRecorder(audio);
-      setMediaRecorder(recorder);
-
-      recorder.start();
-      SpeechRecognition.startListening({ continuous: true, language: 'en-US' });
-    } catch (error: unknown) {
-      console.error('Error accessing microphone: ', error);
-    }
-  };
-
-  const stopRecording = async () => {
-    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
-      mediaRecorder.stop();
-    }
-    SpeechRecognition.stopListening();
-
-    if (audioStream) {
-      audioStream.getTracks().forEach((track) => track.stop());
-      setAudioStream(null);
-    }
-    setMediaRecorder(null);
-    resetTranscript();
-    console.log('Final Transcript:', transcript);
-  };
-
-  const handleToggleRecord = () => {
+  const toggleRecord = () => {
     if (listening) {
-      stopRecording();
+      stopListening();
     } else {
-      startRecording();
+      startListening();
     }
   };
 
@@ -92,33 +45,25 @@ export default function VoiceForm() {
               ready to start recording.
             </DialogDescription>
           </DialogHeader>
-          {!browserSupportsSpeechRecognition ? (
+          {!hasSupport ? (
             <Invalid />
           ) : (
             <>
               <div className="flex flex-col items-center space-y-4">
-                {mediaRecorder && listening && (
-                  <LiveAudioVisualizer
-                    mediaRecorder={mediaRecorder}
-                    width={200}
-                    height={75}
-                  />
-                )}
                 <Button
                   type="button"
                   variant={listening ? 'destructive' : 'default'}
                   className="rounded-full size-12"
-                  onClick={handleToggleRecord}
-                  disabled={!browserSupportsSpeechRecognition}>
+                  onClick={toggleRecord}>
                   <Mic
                     className={`size-5 ${listening ? 'animate-pulse' : ''}`}
                   />
                 </Button>
                 <span className="text-sm text-muted-foreground">
-                  Status: {listening ? 'Listenig...' : 'Idle'}
+                  Status: {listening ? 'Listening...' : 'Idle'}
                 </span>
                 <p className="text-center p-2 text-sm border rounded-md  w-full">
-                  {transcript || 'Start speaking to see the transcription.'}
+                  {text || 'Start speaking to see the transcription.'}
                 </p>
               </div>
               <DialogFooter>
